2ed793b10db80b122eb95c4d01147be6a28c0cd2
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-01-27 12:40:37 +0500

    update dashboard page

--- dashboard/index.php
+++ dashboard/index.php
@@ -2,10 +2,36 @@
 include '../header.php';
 ?>
 
-<div class="container">
+<link rel="stylesheet" href="./index.css">
+
+<div class="hero bg-dark d-flex align-items-end px-5">
+    <h1 class="text-white display-5 fw-bold">Dashboard</h1>
+</div>
+
+<div class="container-fluid bg-dark py-2 px-5">
     <div class="row">
         <div class="col">
-            <h1>Dashboard</h1>
+            <h3 class="text-white fw-bold">Balance: $ 0.00</h3>
+        </div>
+    </div>
+</div>
+
+<div class="container my-5">
+    <div class="row">
+        <div class="col">
+            <h3 class="mb-5"><i class="bi bi-star-fill me-3"></i>Star playing</h3>
+            <a class="btn btn-outline-secondary border-0" href="#" role="button">FIND OPPONENTS</a>
+            <a class="btn btn-outline-secondary border-0" href="#" role="button">CHALLENGES</a>
+            <a class="btn btn-outline-secondary border-0" href="#" role="button">TOURNAMENTS</a>
+            <hr />
+            <a class="btn btn-outline-secondary border-0" href="../help/" role="button">HELP</a>
+            <a class="btn btn-outline-secondary border-0" href="../ranking/" role="button">RANKING</a>
+            <a class="btn btn-outline-secondary border-0" href="#" role="button">DISPUTES</a>
+        </div>
+        <div class="col">
+            <h3 class="mb-5"><i class="bi bi-wallet-fill me-3"></i>Fund your account</h3>
+            <a class="btn btn-outline-secondary border-0" href="#" role="button">MAKE DEPOSIT</a>
+            <a class="btn btn-outline-secondary border-0" href="#" role="button">WITHDRAWAL</a>
         </div>
     </div>
 </div>
