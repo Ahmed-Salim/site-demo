226018b5337d01c7af3df1390165c5481df82551
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-01-27 15:17:16 +0500

    update home page content

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ include './header.php';
 <div class="container-fluid">
     <div id="hero-section" class="row bg-dark">
         <div class="col-8 text-white mx-auto py-5">
-            <h1 class="display-3 fw-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum nisi tincidunt ac arcu a.</h1>
-            <p class="my-4">Curabitur ultricies risus at libero tristique, molestie vestibulum odio posuere.</p>
+            <h1 class="display-3 fw-bold">Win up to $10,000<br />On Fifa 2020<br />Summer Tournament</h1>
+            <p class="my-4">Register for just $10, and get a change to win up to $10,000 in our Fifa 2020 Summer Tournament, sponsored by Redbull</p>
             <a class="btn btn-light btn-lg" href="./register/" role="button">REGISTER NOW</a>
         </div>
     </div>
@@ -17,35 +17,35 @@ include './header.php';
 <div class="container">
     <div class="row my-5">
         <div class="col d-flex align-items-center justify-content-center">
-            <img src="https://picsum.photos/300?random=1" alt="...">
+            <div>
+                <h2>Make your games more exciting</h2>
+                <p>Register for just $10, and get a change to win up to $10,000 in our Fifa 2020 Summer Tournament, sponsored by Redbull</p>
+            </div>
         </div>
         <div class="col d-flex align-items-center justify-content-center">
-            <div>
-                <h2>Sed sed nisi sit amet enim maximus.</h2>
-                <p>Nullam sagittis ornare molestie. Praesent et dui tempor, bibendum leo et, molestie nunc. Aliquam faucibus rutrum ex non dignissim.</p>
-            </div>
+            <img src="https://picsum.photos/300?random=1" alt="...">
         </div>
     </div>
     <div class="row my-5">
-        <div class="col d-flex align-items-center justify-content-center">
-            <div>
-                <h2>Sed sed nisi sit amet enim maximus.</h2>
-                <p>Nullam sagittis ornare molestie. Praesent et dui tempor, bibendum leo et, molestie nunc. Aliquam faucibus rutrum ex non dignissim.</p>
-            </div>
-        </div>
         <div class="col d-flex align-items-center justify-content-center">
             <img src="https://picsum.photos/300?random=2" alt="...">
         </div>
+        <div class="col d-flex align-items-center justify-content-center">
+            <div>
+                <h2>Make money playing your favorites games</h2>
+                <p>Register for just $10, and get a change to win up to $10,000 in our Fifa 2020 Summer Tournament, sponsored by Redbull</p>
+            </div>
+        </div>
     </div>
     <div class="row my-5">
         <div class="col d-flex align-items-center justify-content-center">
-            <img src="https://picsum.photos/300?random=3" alt="...">
+            <div>
+                <h2>Make your games more exciting</h2>
+                <p>Register for just $10, and get a change to win up to $10,000 in our Fifa 2020 Summer Tournament, sponsored by Redbull</p>
+            </div>
         </div>
         <div class="col d-flex align-items-center justify-content-center">
-            <div>
-                <h2>Sed sed nisi sit amet enim maximus.</h2>
-                <p>Nullam sagittis ornare molestie. Praesent et dui tempor, bibendum leo et, molestie nunc. Aliquam faucibus rutrum ex non dignissim.</p>
-            </div>
+            <img src="https://picsum.photos/300?random=3" alt="...">
         </div>
     </div>
 </div>
