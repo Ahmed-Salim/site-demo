63aa956e2b2c4ece405026241fcce1902670f0bf
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-01-28 11:00:28 +0500

    update title and link text

--- header.php
+++ header.php
@@ -41,7 +41,7 @@ if (isset($_SESSION['id'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
-    <title>Demo Site</title>
+    <title>Challenge Site</title>
 </head>
 
 <body>
@@ -53,7 +53,7 @@ if (isset($_SESSION['id'])) {
                         <i class="bi bi-list"></i>
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a class="dropdown-item" href="/demo-site"><i class="bi bi-person-fill me-2"></i>ABC</a></li>
+                        <li><a class="dropdown-item" href="/demo-site"><i class="bi bi-person-fill me-2"></i>Home</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
@@ -108,7 +108,7 @@ if (isset($_SESSION['id'])) {
                     </ul>
                 </li>
             </ul>
-            <a class="navbar-brand" href="/demo-site">ABC</a>
+            <a class="navbar-brand" href="/demo-site">Challenge Site</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
