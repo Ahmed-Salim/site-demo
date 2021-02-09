4f0cf060bd16f16ad79b74028eeccfbc265cd1ae
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-02-09 15:09:15 +0500

    add open challenges list

--- dashboard/find-opponent/index.php
+++ dashboard/find-opponent/index.php
@@ -23,9 +23,41 @@ include '../../header.php';
                     <option value="4">Nintendo</option>
                 </select>
 
-                <button type="button" class="btn btn-outline-secondary border-0 mx-3 text-nowrap">0 CHALLENGES</button>
-                <button type="button" class="btn btn-outline-secondary border-0 mx-3 text-nowrap">0 TOURNAMENTS</button>
-                <button type="button" class="btn btn-outline-secondary border-0 mx-3 text-nowrap">0 PLAYERS</button>
+                <ul class="nav nav-pills nav-justified flex-nowrap mx-3" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active text-uppercase text-nowrap" id="pills-challenges-tab" data-bs-toggle="pill" href="#pills-challenges" role="tab" aria-controls="pills-challenges" aria-selected="true">
+
+                            <?php
+
+                            include_once '../../php-apis/db-config.php';
+                            include_once '../../php-apis/clean-input.php';
+
+                            $user_id = $_SESSION['id'];
+
+                            $sql = "SELECT COUNT(*) AS total_challenges FROM challenges_log WHERE challenge_by <> $user_id";
+                            $result = mysqli_query($conn, $sql);
+
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo $row['total_challenges'];
+                                }
+                            } else {
+                                echo "0";
+                            }
+
+                            ?>
+
+                            Challenges
+                        </a>
+                    </li>
+                    <li class="nav-item mx-2" role="presentation">
+                        <a class="nav-link text-uppercase text-nowrap" id="pills-tournaments-tab" data-bs-toggle="pill" href="#pills-tournaments" role="tab" aria-controls="pills-tournaments" aria-selected="false">Tournaments</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link text-uppercase text-nowrap" id="pills-players-tab" data-bs-toggle="pill" href="#pills-players" role="tab" aria-controls="pills-players" aria-selected="false">Players</a>
+                    </li>
+                </ul>
+
             </div>
         </div>
     </div>
@@ -40,27 +72,90 @@ include '../../header.php';
 <div class="container-fluid my-5 px-5">
     <div class="row">
         <div class="col">
-            <a class="btn btn-light shadow-sm text-uppercase" href="../challenges" role="button">My Challenges</a>
-            <!-- <div class="pt-3 fs-5">
-                <div class="p-3 border">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <span class="fw-bold">FIFA 21 PS4</span>
-                        <div>
-                            <span class="fw-bold">$10</span>
-                            <button class="btn btn-light shadow-sm text-uppercase">Accept</button>
+
+            <div class="tab-content" id="pills-tabContent">
+
+                <div class="tab-pane fade show active" id="pills-challenges" role="tabpanel" aria-labelledby="pills-challenges-tab">
+                    <a class="btn btn-light shadow-sm text-uppercase mb-3" href="../challenges" role="button">My Challenges</a>
+
+                    <?php
+
+                    $sql = "SELECT * FROM users INNER JOIN challenges_log ON users.id = challenges_log.challenge_by WHERE challenges_log.challenge_by <> $user_id";
+                    $result = mysqli_query($conn, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+
+                    ?>
+
+                            <div class="card mb-3">
+                                <h4 class="card-header">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div>
+                                            <?php echo (($row['game'] === 'fifa') ? (strtoupper($row['game'])) : (ucwords(str_replace("_", " ", $row['game'])))) . ' - ' . (($row['console'] === 'ps4' || $row['console'] === 'pc') ? (strtoupper($row['console'])) : (ucwords($row['console']))); ?>
+                                        </div>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <?php echo '$' . $row['amount']; ?>
+                                            <button type="button" class="btn btn-primary text-uppercase ms-2">Accept</button>
+                                        </div>
+                                    </div>
+                                </h4>
+                                <div class="card-body d-flex align-items-center justify-content-between">
+                                    <h5 class="card-title"><i class="bi bi-file-person-fill"></i><?php echo ucwords($row['username']); ?></h5>
+                                    <button type="button" class="btn btn-primary text-uppercase">Details</button>
+                                </div>
+                                <div class="card-footer text-muted"><?php echo 'Created: ' . $row['client_date']; ?></div>
+                            </div>
+
+                    <?php
+
+                        }
+                    } else {
+                        echo "<p class='fs-4 text-danger'>No Challenges Available!</p>";
+                    }
+
+                    ?>
+
+
+                </div>
+
+                <div class="tab-pane fade" id="pills-tournaments" role="tabpanel" aria-labelledby="pills-tournaments-tab">
+                    <a class="btn btn-light shadow-sm text-uppercase mb-3" href="../tournaments" role="button">My Tournaments</a>
+                    <div class="card">
+                        <div class="card-header">
+                            Featured
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                        <div class="card-footer text-muted">
+                            2 days ago
                         </div>
                     </div>
-                    <div class="mb-3">
-                        <i class="bi bi-person-circle fs-3"></i>
-                        <span class="fw-bold">Player 1</span>
-                    </div>
-                    <div class="">
-                        <button class="btn btn-light shadow-sm text-uppercase">Details</button>
-                        <small>Created: 12-12-12</small>
+                </div>
+
+                <div class="tab-pane fade" id="pills-players" role="tabpanel" aria-labelledby="pills-players-tab">
+                    <div class="card">
+                        <div class="card-header">
+                            Featured
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                        <div class="card-footer text-muted">
+                            2 days ago
+                        </div>
                     </div>
                 </div>
-            </div> -->
+
+            </div>
+
         </div>
+
         <div class="col">
             <h3>Shoutbox</h3>
             <form class="row">
