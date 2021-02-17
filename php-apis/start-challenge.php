2119f54a9d32f8e2ef1b0e471c168879202fb6e3
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-02-17 12:55:44 +0500

    fix start challenge if started check bug

--- php-apis/start-challenge.php
+++ php-apis/start-challenge.php
@@ -23,58 +23,63 @@ if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 if ($start_by_id === $row['challenge_by'] || $start_by_id === $row['accepted_by']) {
-                    if ($row['status'] === 'accepted') {
-                        $sql2 = "SELECT NOW() AS now_timestamp";
-                        $result2 = mysqli_query($conn, $sql2);
+                    if (($start_by_id === $row['challenge_by'] && !is_null($row['challenge_by_start_timestamp']) && !empty($row['challenge_by_start_timestamp'])) || ($start_by_id === $row['accepted_by'] && !is_null($row['accepted_by_start_timestamp']) && !empty($row['accepted_by_start_timestamp']))) {
+                        $response_msg['status'] .= 'error';
+                        $response_msg['description'] .= 'Error: You Have Already Started This Challenge!';
+                    } else {
+                        if ($row['status'] === 'accepted') {
+                            $sql2 = "SELECT NOW() AS now_timestamp";
+                            $result2 = mysqli_query($conn, $sql2);
 
-                        if (mysqli_num_rows($result2) > 0) {
-                            while ($row2 = mysqli_fetch_assoc($result2)) {
-                                if (($row['challenge_date'] . ' ' . $row['challenge_time']) >= $row2['now_timestamp']) {
-                                    if ($start_by_id === $row['challenge_by']) {
-                                        $sql5 = "UPDATE challenges_log SET challenge_by_start_timestamp = NOW() WHERE challenge_id = $challenge_id";
-                                    } else {
-                                        $sql5 = "UPDATE challenges_log SET accepted_by_start_timestamp = NOW() WHERE challenge_id = $challenge_id";
-                                    }
+                            if (mysqli_num_rows($result2) > 0) {
+                                while ($row2 = mysqli_fetch_assoc($result2)) {
+                                    if (($row['challenge_date'] . ' ' . $row['challenge_time']) >= $row2['now_timestamp']) {
+                                        if ($start_by_id === $row['challenge_by']) {
+                                            $sql5 = "UPDATE challenges_log SET challenge_by_start_timestamp = NOW() WHERE challenge_id = $challenge_id";
+                                        } else {
+                                            $sql5 = "UPDATE challenges_log SET accepted_by_start_timestamp = NOW() WHERE challenge_id = $challenge_id";
+                                        }
 
-                                    if (mysqli_query($conn, $sql5)) {
-                                        $response_msg['status'] .= 'success';
-                                        $response_msg['description'] .= 'Challenge Started Successfully!';
+                                        if (mysqli_query($conn, $sql5)) {
+                                            $response_msg['status'] .= 'success';
+                                            $response_msg['description'] .= 'Challenge Started Successfully!';
+                                        } else {
+                                            $response_msg['status'] .= 'error';
+                                            $response_msg['description'] .= 'Error: ' . mysqli_error($conn);
+                                        }
                                     } else {
                                         $response_msg['status'] .= 'error';
-                                        $response_msg['description'] .= 'Error: ' . mysqli_error($conn);
-                                    }
-                                } else {
-                                    $response_msg['status'] .= 'error';
-                                    $response_msg['description'] .= 'Error: Challenge Time Is Over!';
+                                        $response_msg['description'] .= 'Error: Challenge Time Is Over!';
 
-                                    $sql3 = "UPDATE challenges_log SET status = 'no_result' WHERE challenge_id = $challenge_id";
+                                        $sql3 = "UPDATE challenges_log SET status = 'no_result' WHERE challenge_id = $challenge_id";
 
-                                    if (mysqli_query($conn, $sql3)) {
-                                        $response_msg['description'] .= 'Challenge Status Updated To No Result!';
-                                    } else {
-                                        $response_msg['description'] .= 'Error: ' . mysqli_error($conn);
-                                    }
+                                        if (mysqli_query($conn, $sql3)) {
+                                            $response_msg['description'] .= 'Challenge Status Updated To No Result!';
+                                        } else {
+                                            $response_msg['description'] .= 'Error: ' . mysqli_error($conn);
+                                        }
 
-                                    $challenge_by = $row['challenge_by'];
-                                    $accepted_by = $row['accepted_by'];
-                                    $amount = $row['amount'];
+                                        $challenge_by = $row['challenge_by'];
+                                        $accepted_by = $row['accepted_by'];
+                                        $amount = $row['amount'];
 
-                                    $sql4 = "UPDATE users SET balance = (balance + $amount) WHERE id = $challenge_by OR id = $accepted_by";
+                                        $sql4 = "UPDATE users SET balance = (balance + $amount) WHERE id = $challenge_by OR id = $accepted_by";
 
-                                    if (mysqli_query($conn, $sql4)) {
-                                        $response_msg['description'] .= 'Challenge Amount Refunded To Both Players!';
-                                    } else {
-                                        $response_msg['description'] .= 'Error: ' . mysqli_error($conn);
+                                        if (mysqli_query($conn, $sql4)) {
+                                            $response_msg['description'] .= 'Challenge Amount Refunded To Both Players!';
+                                        } else {
+                                            $response_msg['description'] .= 'Error: ' . mysqli_error($conn);
+                                        }
                                     }
                                 }
+                            } else {
+                                $response_msg['status'] .= 'error';
+                                $response_msg['description'] .= 'Error: Unknown Query Error!';
                             }
                         } else {
                             $response_msg['status'] .= 'error';
-                            $response_msg['description'] .= 'Error: Unknown Query Error!';
+                            $response_msg['description'] .= 'Error: Challenge Not In Accepted State!';
                         }
-                    } else {
-                        $response_msg['status'] .= 'error';
-                        $response_msg['description'] .= 'Error: Challenge Not In Accepted State!';
                     }
                 } else {
                     $response_msg['status'] .= 'error';
