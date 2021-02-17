fcadb7f7026ef280ed81cd5fcda9b50acfa53ada
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-02-17 16:55:05 +0500

    updated databse added

--- database/codeditors_challenge_site.sql
+++ database/codeditors_challenge_site.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2021 at 12:48 PM
+-- Generation Time: Feb 17, 2021 at 12:54 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -42,23 +42,31 @@ CREATE TABLE `challenges_log` (
   `challenge_date` date DEFAULT NULL,
   `challenge_time` time DEFAULT NULL,
   `challenge_by_start_timestamp` timestamp NULL DEFAULT NULL,
-  `accepted_by_start_timestamp` timestamp NULL DEFAULT NULL
+  `accepted_by_start_timestamp` timestamp NULL DEFAULT NULL,
+  `challenge_by_claimed_result` varchar(255) NOT NULL,
+  `accepted_by_claimed_result` varchar(255) NOT NULL,
+  `challenge_by_claim_timestamp` timestamp NULL DEFAULT NULL,
+  `accepted_by_claim_timestamp` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `challenges_log`
 --
 
-INSERT INTO `challenges_log` (`challenge_id`, `challenge_by`, `game`, `console`, `amount`, `game_mode`, `rules`, `status`, `created_timestamp`, `accepted_by`, `accepted_timestamp`, `challenge_date`, `challenge_time`, `challenge_by_start_timestamp`, `accepted_by_start_timestamp`) VALUES
-(1, 5, 'fifa_21', 'pc', 10, 'Test', 'Test', 'open', '2021-02-08 08:16:32', NULL, NULL, NULL, NULL, NULL, NULL),
-(2, 5, 'fifa_21', 'ps4', 15, '', '', 'open', '2021-02-08 09:44:22', NULL, NULL, NULL, NULL, NULL, NULL),
-(3, 5, 'fortnite', 'xbox', 15, 'Test123', 'Test123', 'accepted', '2021-02-08 11:52:23', 6, '2021-02-16 06:53:36', '2021-02-17', '11:53:00', NULL, NULL),
-(4, 6, 'clash_of_clans', 'xbox', 20, 'Test', 'Test', 'accepted', '2021-02-09 06:17:29', 5, '2021-02-15 11:49:00', '2021-02-16', '16:48:00', NULL, '2021-02-16 11:24:34'),
-(5, 7, 'fifa_21', 'ps4', 10, 'test1', 'test1', 'accepted', '2021-02-09 11:31:56', 5, '2021-02-15 11:29:23', '2021-02-15', '16:29:00', NULL, NULL),
-(6, 7, 'fortnite', 'pc', 15, 'test2', 'test2', 'accepted', '2021-02-09 11:32:32', 5, '2021-02-15 10:52:18', '2021-02-10', '15:52:00', NULL, NULL),
-(7, 7, 'clash_of_clans', 'xbox', 20, 'test3', 'test3', 'accepted', '2021-02-09 11:32:44', 5, '2021-02-15 10:56:16', '2021-02-12', '15:56:00', NULL, NULL),
-(8, 7, 'clash_of_clans', 'nintendo', 25, 'test4', 'test4', 'open', '2021-02-09 11:32:57', NULL, NULL, NULL, NULL, NULL, NULL),
-(9, 5, 'clash_of_clans', 'ps4', 10, 'testing123', 'testing123', 'open', '2021-02-15 06:22:27', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `challenges_log` (`challenge_id`, `challenge_by`, `game`, `console`, `amount`, `game_mode`, `rules`, `status`, `created_timestamp`, `accepted_by`, `accepted_timestamp`, `challenge_date`, `challenge_time`, `challenge_by_start_timestamp`, `accepted_by_start_timestamp`, `challenge_by_claimed_result`, `accepted_by_claimed_result`, `challenge_by_claim_timestamp`, `accepted_by_claim_timestamp`) VALUES
+(1, 5, 'fifa_21', 'pc', 10, 'Test', 'Test', 'open', '2021-02-08 08:16:32', NULL, NULL, NULL, NULL, NULL, NULL, '', '', NULL, NULL),
+(2, 5, 'fifa_21', 'ps4', 15, '', '', 'disputed', '2021-02-08 09:44:22', 11, '2021-02-17 04:35:59', '2021-02-18', '09:35:00', '2021-02-17 07:16:03', '2021-02-17 04:36:42', 'win', 'win', '2021-02-17 11:40:41', '2021-02-17 11:41:17'),
+(3, 5, 'fortnite', 'xbox', 15, 'Test123', 'Test123', 'no_result', '2021-02-08 11:52:23', 6, '2021-02-16 06:53:36', '2021-02-17', '11:53:00', '2021-02-16 11:51:28', NULL, '', '', NULL, NULL),
+(4, 6, 'clash_of_clans', 'xbox', 20, 'Test', 'Test', 'no_result', '2021-02-09 06:17:29', 5, '2021-02-15 11:49:00', '2021-02-16', '16:48:00', NULL, '2021-02-16 11:24:34', '', '', NULL, NULL),
+(5, 7, 'fifa_21', 'ps4', 10, 'test1', 'test1', 'no_result', '2021-02-09 11:31:56', 5, '2021-02-15 11:29:23', '2021-02-15', '16:29:00', NULL, NULL, '', '', NULL, NULL),
+(6, 7, 'fortnite', 'pc', 15, 'test2', 'test2', 'no_result', '2021-02-09 11:32:32', 5, '2021-02-15 10:52:18', '2021-02-10', '15:52:00', NULL, NULL, '', '', NULL, NULL),
+(7, 7, 'clash_of_clans', 'xbox', 20, 'test3', 'test3', 'no_result', '2021-02-09 11:32:44', 5, '2021-02-15 10:56:16', '2021-02-12', '15:56:00', NULL, NULL, '', '', NULL, NULL),
+(8, 7, 'clash_of_clans', 'nintendo', 25, 'test4', 'test4', 'open', '2021-02-09 11:32:57', NULL, NULL, NULL, NULL, NULL, NULL, '', '', NULL, NULL),
+(9, 5, 'clash_of_clans', 'ps4', 10, 'testing123', 'testing123', 'accepted', '2021-02-15 06:22:27', 6, '2021-02-17 07:46:03', '2021-02-18', '12:46:00', '2021-02-17 07:52:07', '2021-02-17 07:52:19', 'win', 'win', '2021-02-17 09:33:35', '2021-02-17 09:32:51'),
+(10, 11, 'fifa_21', 'ps4', 10, 'test', 'test', 'accepted', '2021-02-17 04:35:20', 12, '2021-02-17 05:00:16', '2021-02-18', '10:00:00', '2021-02-17 05:01:35', '2021-02-17 05:02:27', '', '', NULL, NULL),
+(11, 12, 'fifa_21', 'ps4', 10, 'test', 'test', 'open', '2021-02-17 04:59:29', NULL, NULL, NULL, NULL, NULL, NULL, '', '', NULL, NULL),
+(12, 5, 'fortnite', 'nintendo', 25, 'test', 'test', 'completed', '2021-02-17 07:19:49', 6, '2021-02-17 07:20:07', '2021-02-18', '12:20:00', '2021-02-17 07:21:41', '2021-02-17 07:20:42', 'win', 'lose', '2021-02-17 11:43:56', '2021-02-17 11:44:33'),
+(13, 5, 'clash_of_clans', 'pc', 15, 'test', 'test', 'completed', '2021-02-17 11:48:18', 6, '2021-02-17 11:48:44', '2021-02-18', '16:48:00', '2021-02-17 11:49:07', '2021-02-17 11:49:20', 'win', 'lose', '2021-02-17 11:49:36', '2021-02-17 11:49:47');
 
 -- --------------------------------------------------------
 
@@ -103,7 +111,9 @@ INSERT INTO `deposit_log` (`deposit_id`, `deposit_by`, `method`, `amount`, `clie
 (21, 5, 'Dummy Deposit', '1000.000000000000000000000000000000', 'Mon Feb 08 2021 14:44:10 GMT+0500 (Pakistan Standard Time)', '2021-02-08 09:44:10'),
 (22, 7, 'Dummy Deposit', '1000.000000000000000000000000000000', 'Tue Feb 09 2021 16:31:39 GMT+0500 (Pakistan Standard Time)', '2021-02-09 11:31:39'),
 (23, 3, 'Dummy Deposit', '100.000000000000000000000000000000', 'Tue Feb 09 2021 16:52:46 GMT+0500 (Pakistan Standard Time)', '2021-02-09 11:52:46'),
-(24, 5, 'Dummy Deposit', '10.000000000000000000000000000000', 'Mon Feb 15 2021 11:15:28 GMT+0500 (Pakistan Standard Time)', '2021-02-15 06:15:28');
+(24, 5, 'Dummy Deposit', '10.000000000000000000000000000000', 'Mon Feb 15 2021 11:15:28 GMT+0500 (Pakistan Standard Time)', '2021-02-15 06:15:28'),
+(25, 11, 'Dummy Deposit', '100.000000000000000000000000000000', 'Wed Feb 17 2021 09:33:41 GMT+0500 (Pakistan Standard Time)', '2021-02-17 04:33:41'),
+(26, 12, 'Dummy Deposit', '100.000000000000000000000000000000', 'Wed Feb 17 2021 09:58:06 GMT+0500 (Pakistan Standard Time)', '2021-02-17 04:58:06');
 
 -- --------------------------------------------------------
 
@@ -163,11 +173,13 @@ INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `email`, `user
 (2, 'user 2', 'first 2', 'last 2', 'test2@email.com', '$2y$10$T/OyHhWYm9zB/ACaTdmamOEZeOTmTPvRho0VQc4xhd8cOzdJgQutK', '0.000000000000000000000000000000'),
 (3, 'user 3', 'first 3', 'last 3', 'test3@email.com', '$2y$10$FVoMLEU/M0SVFTw2BU6d9Oineo4RobFNTIelJ3HQcBkGWsVw3Vo5y', '85.000000000000000000000000000000'),
 (4, 'user 4', 'first 4', 'last 4', 'test4@email.com', '$2y$10$4a3OLSUtHmewGhh/siaIyuY/CgajvwrsIJ5yf5R.Ch4a/73RaV43e', '25.000000000000000000000000000000'),
-(5, 'user 5', 'first 5', 'last 5', 'test5@email.com', '$2y$10$XKvtqT/BxmE2ZMovzAiiCupkg64hTg58iGgfQYh8CA89T801eGAiy', '966.452222900000000000000000000000'),
-(6, 'user 6', 'first 6', 'last 6', 'test6@email.com', '$2y$10$IOZhOTW3906AtM5HP/zGi.VDE25AbhztbGP3Vkysk1p5tvx6eNOFK', '165.456789900000000000000000000000'),
-(7, 'user 7', 'first 7', 'last 7', 'test7@email.com', '$2y$10$0yitpzEV6ofiGi9npHatYO7RbbtWTN.HBaMABQK/kjNMu4U9cwfcy', '860.000000000000000000000000000000'),
+(5, 'user 5', 'first 5', 'last 5', 'test5@email.com', '$2y$10$XKvtqT/BxmE2ZMovzAiiCupkg64hTg58iGgfQYh8CA89T801eGAiy', '1086.452222900000000000000000000000'),
+(6, 'user 6', 'first 6', 'last 6', 'test6@email.com', '$2y$10$IOZhOTW3906AtM5HP/zGi.VDE25AbhztbGP3Vkysk1p5tvx6eNOFK', '150.456789900000000000000000000000'),
+(7, 'user 7', 'first 7', 'last 7', 'test7@email.com', '$2y$10$0yitpzEV6ofiGi9npHatYO7RbbtWTN.HBaMABQK/kjNMu4U9cwfcy', '905.000000000000000000000000000000'),
 (8, 'user 8', 'first 8', 'last 8', 'test07@email.com', '$2y$10$5KKCpf7ER6wMP.lRIZIC5u/HSwldm80iTORIIU0rdIvvNZ/oe7hK6', '0.000000000000000000000000000000'),
-(10, 'user new', 'first', 'last', 'new@email.com', '$2y$10$AE0y6PiCn3m/e2iu71rh6.QqGC/665Zkx8t3FeVoSL/FsULmTzusW', '0.000000000000000000000000000000');
+(10, 'user new', 'first', 'last', 'new@email.com', '$2y$10$AE0y6PiCn3m/e2iu71rh6.QqGC/665Zkx8t3FeVoSL/FsULmTzusW', '0.000000000000000000000000000000'),
+(11, 'hello', 'hello', 'world', 'hello@world.com', '$2y$10$MVcurLqKIS9sPxCO.BW2ZuqLiDuqZYIbL3bFIiH1KaDS8G0qh7A0q', '55.000000000000000000000000000000'),
+(12, 'hello world', 'hello', 'world', 'hello@email.com', '$2y$10$oBGKi1yC6aDis/vUAsS9/eC0zWkoG5Lifp8vZytpLtUTcZoHDbWfi', '60.000000000000000000000000000000');
 
 -- --------------------------------------------------------
 
@@ -201,7 +213,9 @@ INSERT INTO `withdrawal_log` (`withdrawal_id`, `withdrawal_by`, `method`, `amoun
 (9, 5, 'Dummy Withdrawal', '25.000000000000000000000000000000', 'Success', 'Sat Feb 06 2021 16:47:27 GMT+0500 (Pakistan Standard Time)', '2021-02-06 11:47:27'),
 (10, 5, 'Dummy Withdrawal', '28.000000000000000000000000000000', 'Success', 'Sat Feb 06 2021 16:57:01 GMT+0500 (Pakistan Standard Time)', '2021-02-06 11:57:01'),
 (11, 5, 'Dummy Withdrawal', '98.765432100000000000000000000000', 'Success', 'Sat Feb 06 2021 17:01:25 GMT+0500 (Pakistan Standard Time)', '2021-02-06 12:01:25'),
-(12, 5, 'Dummy Withdrawal', '20.000000000000000000000000000000', 'Success', 'Mon Feb 15 2021 11:15:44 GMT+0500 (Pakistan Standard Time)', '2021-02-15 06:15:44');
+(12, 5, 'Dummy Withdrawal', '20.000000000000000000000000000000', 'Success', 'Mon Feb 15 2021 11:15:44 GMT+0500 (Pakistan Standard Time)', '2021-02-15 06:15:44'),
+(13, 11, 'Dummy Withdrawal', '20.000000000000000000000000000000', 'Success', 'Wed Feb 17 2021 09:34:36 GMT+0500 (Pakistan Standard Time)', '2021-02-17 04:34:36'),
+(14, 12, 'Dummy Withdrawal', '20.000000000000000000000000000000', 'Success', 'Wed Feb 17 2021 09:58:45 GMT+0500 (Pakistan Standard Time)', '2021-02-17 04:58:45');
 
 --
 -- Indexes for dumped tables
@@ -245,13 +259,13 @@ ALTER TABLE `withdrawal_log`
 -- AUTO_INCREMENT for table `challenges_log`
 --
 ALTER TABLE `challenges_log`
-  MODIFY `challenge_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `challenge_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `deposit_log`
 --
 ALTER TABLE `deposit_log`
-  MODIFY `deposit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `deposit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `tournaments_log`
@@ -263,13 +277,13 @@ ALTER TABLE `tournaments_log`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `withdrawal_log`
 --
 ALTER TABLE `withdrawal_log`
-  MODIFY `withdrawal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `withdrawal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
