cada85fe46f0dafc80d241f1c75cc7a74679de39
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-02-24 09:28:03 +0500

    add accept challenge confirm box

--- dashboard/find-opponent/index1.js
+++ dashboard/find-opponent/index1.js
@@ -2,36 +2,39 @@ const acceptChallengeButtons = document.querySelectorAll('.accept-challenge-butt
 
 acceptChallengeButtons.forEach(function (acceptChallengeButton, currentIndex, listObj) {
     acceptChallengeButton.addEventListener('click', () => {
-        const XHR = new XMLHttpRequest();
-        const FD = new FormData();
+        if (window.confirm("Are you sure you want to Accept this Challenge ?\nAccepting this Challenge will DEDUCT the Challenge ammount from your Balance.\nPress (OK) to Accept or (Cancel) to return back.")) {
+            const XHR = new XMLHttpRequest();
+            const FD = new FormData();
 
-        // Push our data into our FormData object
-        FD.append('challenge-id', acceptChallengeButton.dataset.challengeId);
+            // Push our data into our FormData object
+            FD.append('challenge-id', acceptChallengeButton.dataset.challengeId);
 
-        acceptChallengeButton.disabled = true;
+            acceptChallengeButton.disabled = true;
 
-        // Define what happens on successful data submission
-        XHR.addEventListener('load', function (event) {
-            alert(JSON.parse(event.target.responseText).description);
+            // Define what happens on successful data submission
+            XHR.addEventListener('load', function (event) {
+                alert(JSON.parse(event.target.responseText).description);
 
-            if (JSON.parse(event.target.responseText).status === 'success') {
-                window.location.href = "../challenges";
-            } else {
+                if (JSON.parse(event.target.responseText).status === 'success') {
+                    window.location.href = "../challenges";
+                } else {
+                    acceptChallengeButton.disabled = false;
+                }
+            });
+
+            // Define what happens in case of error
+            XHR.addEventListener('error', function (event) {
+                alert('Oops! Something went wrong.');
+
                 acceptChallengeButton.disabled = false;
-            }
-        });
+            });
 
-        // Define what happens in case of error
-        XHR.addEventListener('error', function (event) {
-            alert('Oops! Something went wrong.');
+            // Set up our request
+            XHR.open('POST', '../../php-apis/accept-challenge.php');
 
-            acceptChallengeButton.disabled = false;
-        });
-
-        // Set up our request
-        XHR.open('POST', '../../php-apis/accept-challenge.php');
-
-        // Send our FormData object; HTTP headers are set automatically
-        XHR.send(FD);
+            // Send our FormData object; HTTP headers are set automatically
+            XHR.send(FD);
+        } else {
+        }
     });
 });
