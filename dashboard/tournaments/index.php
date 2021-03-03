3d91207328af7564d079cc580ad642a06996b67b
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-03-03 16:49:29 +0500

    add open tournaments cards

--- dashboard/tournaments/index.php
+++ dashboard/tournaments/index.php
@@ -84,6 +84,36 @@
     </div>
 </div>
 
+<!-- Tourney Players Modal -->
+<div class="modal fade" id="tourney-players-modal" tabindex="-1" aria-labelledby="tourney-players-modal-label" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tourney-players-modal-label"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Player Username</th>
+                            <th scope="col">Skill Level</th>
+                            <th scope="col">Skill Points</th>
+                            <th scope="col">Entered Timestamp</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="hero d-flex align-items-end">
     <h1 class="ms-5 text-white display-5 fw-bold">My Tournaments</h1>
 </div>
@@ -102,7 +132,7 @@
 <div class="container my-5">
     <div class="row">
         <div class="col">
-            <p class="fs-1">
+            <h2 class="text-center fs-1">
 
                 <?php
 
@@ -125,17 +155,79 @@
                 ?>
 
                 Open
-            </p>
+            </h2>
+
+            <hr />
+
+            <?php
+
+            $sql4 = "SELECT * FROM tournaments_log WHERE tournament_by = $user_id AND status = 'open' ORDER BY created_timestamp DESC";
+            $result4 = mysqli_query($conn, $sql4);
+
+            if (mysqli_num_rows($result4) > 0) {
+                while ($row4 = mysqli_fetch_assoc($result4)) {
+
+            ?>
+
+                    <div class="card my-5">
+                        <div class="card-header">
+                            Tournament # <?php echo $row4['tournament_id']; ?>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <?php echo (($row4['game'] === 'fifa_21') ? (strtoupper(str_replace("_", " ", $row4['game']))) : (ucwords(str_replace("_", " ", $row4['game'])))) . ' - ' . (($row4['console'] === 'ps4' || $row4['console'] === 'pc') ? (strtoupper($row4['console'])) : (ucwords($row4['console']))); ?>
+                            </h5>
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Amount</th>
+                                        <td><?php echo '$' . $row4['amount']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Players</th>
+                                        <td><?php echo $row4['players']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Start Date</th>
+                                        <td><?php echo $row4['start_date'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Start Time</th>
+                                        <td><?php echo $row4['start_time']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Game Mode</th>
+                                        <td><?php echo $row4['game_mode']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Rules</th>
+                                        <td><?php echo $row4['rules']; ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tourney-players-modal" data-bs-tourneyId="<?php echo $row4['tournament_id']; ?>">
+                                Players
+                            </button>
+                        </div>
+                        <div class="card-footer text-muted">
+                            Created: <?php echo $row4['created_timestamp']; ?>
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+            <?php } else { ?>
+
+                <h3 class="text-center text-danger">No Open Tournaments</h3>
+
+            <?php } ?>
+
         </div>
         <div class="col">
-            <p class="fs-1">
+            <p class="fs-1 text-center">
 
                 <?php
-
-                include_once '../../php-apis/db-config.php';
-                include_once '../../php-apis/clean-input.php';
-
-                $user_id = $_SESSION['id'];
 
                 $sql2 = "SELECT COUNT(*) AS confirmed_count FROM tournaments_log WHERE tournament_by = $user_id AND status = 'confirmed'";
                 $result2 = mysqli_query($conn, $sql2);
@@ -154,13 +246,9 @@
             </p>
         </div>
         <div class="col">
-            <p class="fs-1">
+            <p class="fs-1 text-center">
 
                 <?php
-
-                include_once '../../php-apis/db-config.php';
-
-                $user_id = $_SESSION['id'];
 
                 $sql3 = "SELECT COUNT(*) AS reported_count FROM tournaments_log WHERE tournament_by = $user_id AND status = 'reported'";
                 $result3 = mysqli_query($conn, $sql3);
--- dashboard/tournaments/index1.js
+++ dashboard/tournaments/index1.js
@@ -5,6 +5,98 @@ const createTourneyButton = document.querySelector('button[form="create-tourney-
 const tourneyGameSelect = document.querySelector('#tourney-game');
 var createTourneyModal = document.getElementById('createTourneyModal');
 
+var tourneyPlayersModal = document.getElementById('tourney-players-modal');
+
+tourneyPlayersModal.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    var button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    var tourneyId = button.getAttribute('data-bs-tourneyId');
+    // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+
+    const XHR = new XMLHttpRequest();
+    const FD = new FormData();
+
+    // Push our data into our FormData object
+    FD.append('tourney-id', tourneyId);
+
+    // Define what happens on successful data submission
+    XHR.addEventListener('load', function (event) {
+        let tourneyPlayersModalTitle = tourneyPlayersModal.querySelector('.modal-title');
+        tourneyPlayersModalTitle.innerHTML = '';
+        let tourneyPlayersTbody = tourneyPlayersModal.querySelector('.modal-body tbody');
+        tourneyPlayersTbody.innerHTML = '';
+
+        if (JSON.parse(event.target.responseText).status === 'success') {
+            let response_msg = JSON.parse(event.target.responseText);
+            let tourney_details = response_msg.tourney_details;
+
+            let gameConsole = ((tourney_details.game === 'fifa_21') ? (tourney_details.game.replaceAll("_", " ").toUpperCase()) : (tourney_details.game.replaceAll("_", " ").toUpperCase())) + ' - ' + ((tourney_details.console === 'ps4' || tourney_details.console === 'pc') ? (tourney_details.console.toUpperCase()) : (tourney_details.console.toUpperCase()));
+
+            tourneyPlayersModalTitle.textContent = 'Tournament # ' + tourney_details.tournament_id + ' (' + gameConsole + ')';
+
+            tourney_details.tourney_players.forEach((tourney_player, index) => {
+                let tr = document.createElement('tr');
+
+                let td1 = document.createElement('td');
+                td1.textContent = (index + 1);
+                tr.appendChild(td1);
+
+                let td2 = document.createElement('td');
+                td2.textContent = tourney_player.username.toUpperCase();
+                tr.appendChild(td2);
+
+                let td3 = document.createElement('td');
+                td3.textContent = tourney_player.skill_level.toUpperCase();
+                tr.appendChild(td3);
+
+                let td4 = document.createElement('td');
+                td4.textContent = tourney_player.skill_points;
+                tr.appendChild(td4);
+
+                let td5 = document.createElement('td');
+                td5.textContent = tourney_player.enter_timestamp;
+                tr.appendChild(td5);
+
+                tourneyPlayersTbody.appendChild(tr);
+            });
+        } else {
+            let errorSpan = document.createElement('span');
+            errorSpan.classList.add('text-danger');
+            errorSpan.textContent = 'Error!';
+
+            tourneyPlayersModalTitle.appendChild(errorSpan);
+
+            let response_msg = JSON.parse(event.target.responseText);
+
+            response_msg.error_msgs.forEach((error_msg) => {
+                let tr = document.createElement('tr');
+                let td = document.createElement('td');
+                td.setAttribute('colSpan', 5);
+                td.classList.add('text-center');
+                td.classList.add('text-danger');
+                td.textContent = error_msg;
+
+                tr.appendChild(td);
+                tourneyPlayersTbody.appendChild(tr);
+            });
+        }
+    });
+
+    // Define what happens in case of error
+    XHR.addEventListener(' error', function (event) {
+        alert('Oops! Something went wrong.');
+    });
+
+    // Set up our request
+    XHR.open('POST', '../../php-apis/get-tourney-details.php');
+
+    // Send our FormData object; HTTP headers are set automatically
+    XHR.send(FD);
+});
+
 createTourneyModal.addEventListener('shown.bs.modal', function (event) {
     let today = new Date();
     tourneyStartDate.setAttribute('min', new Date(today.setDate(today.getDate() + 1)).toISOString().slice(0, 10));
